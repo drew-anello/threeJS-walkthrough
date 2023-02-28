a3c4c3bc6ef32e27088da924dd5d66599bae3c73
Drew Anello <drew.anello@gmail.com>
2023-02-28 12:37:43 -0500

    adds camera

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 import * as THREE from 'three'
 
+// scene (think of this as a movie scene this is the enviorment)
 const scene = new THREE.Scene()
 
+// creating the sphere (this is what will create the shape and texture)
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: '#00ff83'
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+// camera
+// params are field of view
+const camera = new THREE.PerspectiveCamera(45, 800, 600)
+scene.add(camera)
