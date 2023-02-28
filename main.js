075fff27c93bb27a61c79b455fa055bdd53ef047
Drew Anello <drew.anello@gmail.com>
2023-02-28 15:13:17 -0500

    adds styling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import './style.css'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import gsap from 'gsap'
 // scene (think of this as a movie scene this is the enviorment)
 const scene = new THREE.Scene()
 
@@ -65,3 +66,9 @@ const loop = () => {
   window.requestAnimationFrame(loop)
 }
 loop()
+
+//  timeline
+const tl = gsap.timeline({ defaults: { duration: 1 } })
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 })
+tl.fromTo('nav', { y: '-100%' }, { y: '0%' })
+tl.fromTo('.title', { opacity: '0' }, { opacity: '1' })
