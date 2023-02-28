a9fcd710ebb2b3489ac9fc40ace60cf0ab9cf952
Drew Anello <drew.anello@gmail.com>
2023-02-28 14:29:31 -0500

    formats the size

--- main.js
+++ main.js
@@ -12,18 +12,30 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+//  sizes
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
 // lights
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(0, 10, 10)
 scene.add(light)
 
 // camera (params are field of view)
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
 camera.position.z = 20
 scene.add(camera)
 
 // renderer
 const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({ canvas })
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
